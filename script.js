150da517c793cf1860c804d66a36a009d2ff0043
Tshepang Taele <tshepangtaele09@gmail.com>
2025-06-26 00:22:11 +0200

    JS layout and removing 0 in the display

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
 //Refrence display element
 const display = document.getElementById('display');
 
-//Tracj=k if we have performed a calculation
+//Track if we have performed a calculation
 let justCalculated = false;
 
-function appendTODisplay(value){
+function appendToDisplay(value){
     console.log('Button pressed: ', value);
+
+    let  currentValue = display.value;
+
+    if(justCalculated && !isNaN(value)){
+        display.value = value;
+        justCalculated = false;
+        return;
+    }
+
+    //If current display show 0 and user enters a anumber, we wanna replace the 0
+    if(currentValue === '0' && !isNaN(value)){
+        display.value = value;
+    }
+    else if (currentValue === '0' && value === '.' ) {
+        display.value = currentValue + value;
+    }
+    else {
+        display.value = currentValue + value;
+    }
+
+    //Reset the justCalcu;ated f;ag when user starts typing
+    justCalculated = false;
+
+    console.log('Display updated to: ', display.value);
+
+    
     
     alert('You pressed: ' + value);
 }
 
-function clearDislpay(){
+function clearDisplay(){
     console.log('Clear button pressed: ');
 
     alert('clear button was clicked');
 }
 
-function Brakects(){
+function Brackets(){
     console.log('Brackets button pressed: ');
 
     alert('brackets button was clicked');
@@ -28,10 +54,20 @@ function Percentage(){
     alert('percentage button was clicked');
 }
 
-function delectLast(){
-    console.log('Delect button pressed: ');
+function deleteLast(){
+    console.log('Backspace button pressed: ');
 
-    alert('delect button was clicked');
+    let current = display.value;
+    
+    //if theres only one character or its 0, reset to 0
+    if(currentValue.length <= 1 || currentValue === '0'){
+        display.value = '0';
+    }
+    else{
+        display.value = currentValue.slice(0, -1); 
+    }
+
+    alert('backspace button was clicked');
 }
 
 function Calculate(){
